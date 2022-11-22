01488124d57527249b1b97b91589007e9022343f
Rawllings Kiprop <rutojr01@gmail.com>
2022-11-22 11:12:52 +0300

    Phase 1 Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+let num1 = 2;
+let num2 =31;
 
 // Write your code here
+
+// is an equation whose multiplied results will equal 62"
+
+const multiply = num1 * num2;
+multiply;
+
+// generates a random integer greater than 0 
+
+const random = Math.floor(Math.random() * 10) + 1;
+random;
+
+// is an equation that calculates a remainder that is equal to 4 
+
+let num3 = 20;
+let num4 = 8;
+const mod = num3 % num4;
+mod;
+
+// will return 20 as the highest number in the set 
+const max = Math.max (0, 5, 10, 15, 20);
+max;
